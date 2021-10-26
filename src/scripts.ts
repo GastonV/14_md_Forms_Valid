4884e0e6ed11d9a3d0c1692fac84532fbc5ef1a5
GastonV <atis.sirmais@gmail.com>
2021-10-26 14:15:37 +0300

    popup show

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,27 +1,61 @@
 const catchMyTag = document.querySelector(".form__userName__input");
 const btn_submit = document.querySelector(".submit__input");
 const form: HTMLFormElement = document.querySelector("form");
-let classes = (classes: string) => document.getElementsByClassName(classes);
-
+//let classes = (classes: string) => document.getElementsByClassName(classes);
+//const button__dropdown = document.querySelector(".button__dropdown");
+//const dropdwon = document.querySelector(".js_dropdown");
+//const dropdownClassList = dropdwon.classList;
+let errorMessage = "";
+let checkValue = "";
 const validation = {
   username: (value: string): boolean => {
-    return minMaxValid(value);
+    if (!minMaxValid(value)) {
+      errorMessage =
+        "Your username is too short or too long pleace choose between 4 < length > 12 chars long";
+      return false;
+    }
+    return true;
   },
   password: (value: string): boolean => {
-    return minMaxValid(value);
+    checkValue = value;
+    if (!minMaxValid(value)) {
+      errorMessage =
+        "Your password is too short or too long pleace choose between 4 < length > 12 chars long";
+      return false;
+    }
+    return true;
   },
   password_confirm: (value: string): boolean => {
-    return minMaxValid(value);
+    if (checkValue !== value) {
+      errorMessage = "Your password does not match";
+      return false;
+    }
+    if (value.length < 4) {
+      errorMessage = "Too short";
+      return false;
+    }
+    return true;
   },
   selective_choice: (): boolean => {
     return true;
   },
-  radio: (): boolean => {
-    return true;
+  radio: (value: string): boolean => {
+    return value !== "";
   },
 };
 
 type inputKeys = keyof typeof validation;
+
+// button__dropdown.addEventListener("click", (e) => {
+//   dropdwon.classList.toggle("show");
+// });
+// window.addEventListener("click", (event) => {
+//   if (event.target !== button__dropdown) {
+//     if (dropdownClassList.contains("show")) {
+//       dropdownClassList.remove("show");
+//     }
+//   }
+// });
 
 const minMaxValid = (inputString: string): boolean => {
   let min = 4;
@@ -32,17 +66,29 @@ const minMaxValid = (inputString: string): boolean => {
 form.addEventListener("submit", (e) => {
   //prevents default browser actions
   e.preventDefault();
+  let selectInputSpanTags =
+    document.getElementsByClassName("input__span__info");
   const formValues = new FormData(form);
-
-  if (form.checkValidity()) {
-    formValues.forEach((value: string, classNameKey: inputKeys) => {
-      console.log(classNameKey + "      class name accessss");
-      console.log(value + "      class name accessss");
-      let isValidForm = validation[classNameKey](value);
-      if (!isValidForm) {
-        console.error(value + "   was incorect");
-      }
-      // do the magic with css
-    });
-  }
+  let index = 0;
+  formValues.forEach((value: string, classNameKey: inputKeys) => {
+    let isValidForm = validation[classNameKey](value);
+    if (isValidForm) {
+      const popup = document.querySelector(".pop_up_sucess");
+      popup.classList.add("popup_show");
+     
+    }
+    if (!isValidForm) {
+      selectInputSpanTags[index].innerHTML = errorMessage;
+      selectInputSpanTags[index].classList.add("show", "show--red");
+    } else {
+      //@ts-ignore
+      selectInputSpanTags[index].classList.remove("show", "show--red");
+    }
+    index++;
+  });
+});
+window.addEventListener("click", e =>{
+  const outsideClick = document.querySelector(".sucess__screen");
+  outsideClick.classList.contains("popup_show");
+  
 });
